7eec1cdc45562f6e3d2c6a86c6def347e302255d
Andrej <andriuska96@gmail.com>
2017-11-10 14:40:23 +0000

    improve
    improve

--- system/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class stock(models.Model):
-    album= models.CharField(max_length=250)
-    album_title = models.CharField(max_length=300)
-    genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=1000)
-
-
