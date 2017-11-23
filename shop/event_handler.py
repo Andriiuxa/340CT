94485024ce9fa794dbe8464a5ca08afd4de23b1b
Andrej <andriuska96@gmail.com>
2017-11-23 15:39:35 +0000

    adding comments, tweaking

--- shop/event_handler.py
+++ shop/event_handler.py
@@ -14,3 +14,36 @@ class EventHandler:
             url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
         ]
         return (urlpatterns[])
+
+    #LISTING PRODUCT DETAILS REQUEST
+    def product_detail(request, id, slug):
+        product = get_object_or_404(Product, id=id, slug=slug, available=True)
+        return render(request,
+                      'shop/product/detail.html',
+                      {'product': product})
+
+    #LISTING THE PRODUCTS REQUEST
+    def product_list(request, category_slug=None):
+        category = None
+        categories = Category.objects.all()
+        products = Product.objects.filter(available=True)
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
+        return render(request, 'shop/product/list.html', {'category': category,
+                                                          'categories': categories,
+                                                          'products': products})
+
+    #LISTING ALL OF THE OPTIONS FOR THE CATEGORY ADMIN REQUEST
+    def CategoryAdmin(admin.ModelAdmin):
+        list_display = ['name', 'slug']
+        prepopulated_fields = {'slug': ('name',)}
+        return admin.site.register(Category, CategoryAdmin)
+
+    #LISTING ALL OF THE OPTIONS FOR THE PRODUCT ADMIN REQUEST
+    def ProductAdmin(admin.ModelAdmin):
+        list_display = ['name', 'slug', 'category', 'price', 'stock', 'available', 'created', 'updated']
+        list_filter = ['available', 'created', 'updated', 'category']
+        list_editable = ['price', 'stock', 'available']
+        prepopulated_fields = {'slug': ('name',)}
+        return admin.site.register(Product, ProductAdmin)
--- shop/graph.js
+++ shop/graph.js
@@ -1,4 +1,4 @@
-//GENERATING A GRAPH USING JSAPI FROM GRAPHOS 
+//GENERATING A GRAPH USING JSAPI FROM GRAPHOS
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script type="text/javascript">
     google.load("visualization", "1", {packages:["corechart"]});
